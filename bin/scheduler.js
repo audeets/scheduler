c677b607bc3d64948d6d55017e5bd7e112e073da
Benoit Quette <benoit.quette@gmail.com>
2023-12-08 13:02:43 +0100

    fixed fetch

--- bin/scheduler.js
+++ bin/scheduler.js
@@ -11,7 +11,7 @@ const _ = require('lodash');
 const cron = require('node-cron');
 const amqp = require('amqplib/callback_api');
 const http = require('http');
-const URL = require('url');
+const URL = require('url').Url;
 
 // End of dependencies.
 
@@ -26,34 +26,6 @@ cron.schedule(crawlerConfig.schedule, () => {
 console.log('scheduler started');
 
 /**
- * Retrieves the list of projects fron the Projects API.
- * @param {*} callback the callback to return the projects
- */
-function getProjects(callback) {
-  const options = new URL(`${config.hosts.apiProjects}/api/projects`);
-  console.log('preparing GET request to projects API...');
-  const req = http.request(options, res => {
-    console.log('getting response');
-    res.setEncoding('utf8');
-    let responseString = '';
-    res.on('data', chunk => {
-      console.log(`got chunk ${chunk}`);
-      responseString += chunk;
-    });
-    res.on('end', () => {
-      console.log('reponse ended.');
-      callback(null, JSON.parse(responseString));
-    });
-  });
-
-  req.on('error', e => {
-    callback(`problem with request: ${e.message}`);
-  });
-
-  req.end();
-}
-
-/**
  * Loads all the projects from the db and run the audit of each of them by
  * creating a crawling task.
  */
@@ -66,21 +38,23 @@ function audit() {
       if (err) console.log(err);
       console.log('connected to channel');
       ch.assertExchange(EXCHANGE, 'fanout', {durable: true});
-      console.log('exchange is ok. now getting projects');
       // loop through the projects and send a crawl task
-      getProjects((err, projects) => {
-        if (err) console.log(err);
-        console.log(`found ${projects.length} projects`);
-        return _.each(projects, project => {
-          const message = JSON.stringify({
-            url: project.url,
-            project: project._id, // eslint-disable-line
-            nodeUrl
+      let url = `${config.hosts.apiProjects}/api/projects`;
+      console.log(`exchange is ok. now getting projects from ${url}`);
+      fetch(url)
+        .then(response => response.json())
+        .then(projects => {
+          _.each(projects, project => {
+            const message = JSON.stringify({
+              url: project.url,
+              project: project._id, // eslint-disable-line
+              nodeUrl
+            });
+            ch.publish(EXCHANGE, '', Buffer.from(message));
+            console.log(`message published: ${message}`);
           });
-          ch.publish(EXCHANGE, '', new Buffer(message));
-          console.log(`message published: ${message}`);
-        });
-      });
+        })
+        .catch(error => console.log(error));
       setTimeout(() => { // close the rabbitmq connection
         conn.close();
       }, 5000);
